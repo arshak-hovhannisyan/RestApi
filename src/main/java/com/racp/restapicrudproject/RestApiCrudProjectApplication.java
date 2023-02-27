ec16027a06037a3185fa1333e16292fbc5d05de1
arshak-hovhannisyan <arshak.hovhannisyan.94@gmail.com>
2023-02-27 18:57:00 +0400

    Start

---
+++ src/main/java/com/racp/restapicrudproject/RestApiCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.racp.restapicrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCrudProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/racp/restapicrudproject/RestApiCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racp.restapicrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
